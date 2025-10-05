1bcd8e20a31dc47df606217952d1baaebfd3770c
TheBOSSNX <alcarvalho3301@gmail.com>
2025-10-05 15:53:27 -0400

    english forms

--- nasaTabela.php
+++ nasaTabela.php
@@ -3,6 +3,11 @@
 header("Access-Control-Allow-Origin: *"); // permite qualquer origem, ou substitua pelo seu domínio específico
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 include('./db.php');
 
 // Consulta para selecionar os dados geoespaciais e convertê-los em GeoJSON
